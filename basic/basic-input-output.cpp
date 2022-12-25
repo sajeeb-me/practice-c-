04b583f75ccddcd3d3c3e1e41fdf3f8c3f708fa8
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2022-12-25 13:21:43 +0600

    basic input output

---
+++ basic/basic-input-output.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+//    int x,y;
+//
+//    cin >> x >> y;
+//    cout << x << " " << y << "\n"
+//
+//    char ch;
+//    float f;
+//    double d;
+//
+//    cin >> ch >> f >> d;
+//    cout << ch << " " << f << " " << d << "\n";
+
+    string s = "This is ";
+    string s2 = "Shuvo.";
+    s=s+s2;
+//    cin >> s;
+    cout << s << "\n";
+
+    return 0;
+}
