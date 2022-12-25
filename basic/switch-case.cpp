1d7082adfe236fe3a3a4478eb078788e6ed4af18
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2022-12-25 16:04:30 +0600

    switch case

---
+++ basic/switch-case.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    switch(x)
+    {
+    case 2:
+        cout << "Number: 2\n";
+        break;
+    case 5:
+        cout << "Number: 5\n";
+        break;
+    default:
+        cout << "Invalid\n";
+        break;
+    }
+
+    return 0;
+}
