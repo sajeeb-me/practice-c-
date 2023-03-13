68697e5b412cb06765068423f70aed45addb5918
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2023-03-13 10:27:39 +0600

    recursion isPalidrom

---
+++ New folder/recursion-is-palilndrom.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string str)
+{
+    if(str == "" || str.size() == 1)
+        return true;
+    int n = str.size();
+    string small_str = str.substr(1, n-2);
+    return isPalindrome(small_str) && (str[0] == str.back());
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+    cout << (isPalindrome(str) ? "True" : "False");
+
+    return 0;
+}
